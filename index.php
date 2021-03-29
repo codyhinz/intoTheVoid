ce2f0e2b1b054e65466ab990b3414ea59beaa8be
Cody Hinz <codyhinz@gmail.com>
2021-03-28 21:50:00 -0400

    Initial stylizing and navigation introduced

--- index.php
+++ index.php
@@ -1,25 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <link rel="stylesheet" href="./view/style.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <title>Into the Void</title>
-    </head>
-    <body>
-        <nav class="navbar navbar-dark border rounded border-light">
-            <div class="container-fluid">
-                <div class="border-right border-light padding-0 align-right">
-                    <a class="navbar-brand align-right" href="#">
-                        <img src="./view/shadowformicon.jpg" alt="" width="35" height="35" class="d-inline-block border border-light">
-                    </a>
-                </div>
-            </div>
-        </nav>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./view/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <title>Into the Void</title>
+</head>
+<body>
+    <div class="container-fluid navrow">
+        <div class="row">
+            <img src="./view/shadowformicon.jpg" width="37" height="35" margin-left="0">
+            <div class="heading navigation col-2" id="active"><a href="#" class="stretched-link">Home</a></div>
+            <div class="heading navigation col-2"><a href="#">PVE Guides</a></div>
+            <div class="heading navigation col-2"><a href="#">Best In Slot List</a></div>
+            <div class="heading navigation col-2"><a href="#">Spell Power Calculator</a></div>
+            <div class="col-1"></div>
+            <div class="heading col-2">Hey there stranger</div>
+            <script>
+                const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+                const height = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
+                console.log(width, height);
+            </script>
+        </div>
+    </div>
+</body>
 </html>
