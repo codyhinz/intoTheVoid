5e49b911cc416b426a54844bbed1d43a64cf9d2f
Cody Hinz <codyhinz@gmail.com>
2021-03-28 23:13:24 -0400

    Updated styling quite a bit, Finished most of the homepage

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="./view/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Courgette">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merienda">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -22,6 +24,11 @@
             <div class="col-1"></div>
             <div class="heading col-2">Hey there stranger</div>
         </div>
+    </div>
+    <div class="container introduction">
+        <h2>Welcome Home</h2><hr/>
+        <p>This is our shadowy lair. We will be journeying into the dark portal soon enough, and lucky for you Blizzard has blessed us into viability. I plan on playing a Draenei Shadow Priest in The Burning Crusade Classic, and I can't wait for the prepatch to hit live servers. This website is dedicated to all lovers of the world of shadows (no warlocks allowed, sorry). Join me as I attempt to create the best encapsulation of all things Shadow Priest to help your journey into The Burning Crusade Classic.
+        </p>
     </div>
 </body>
 </html>
