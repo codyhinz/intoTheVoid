4a06849d29c418c21a135166c4150740198fb527
Cody Hinz <codyhinz@gmail.com>
2021-03-30 12:09:45 -0400

    Added php and a few changes

--- index.php
+++ index.php
@@ -1,34 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./view/style.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Courgette">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merienda">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <title>Into the Void</title>
-</head>
-<body>
-    <div class="container-fluid navrow">
-        <div class="row">
-            <img src="./view/shadowformicon.jpg" width="37" height="35" margin-left="0" alt="Shadow Form Icon">
-            <div class="heading navigation col-2" id="active"><a href="#" class="stretched-link">Home</a></div>
-            <div class="heading navigation col-2"><a href="#">PVE Guides</a></div>
-            <div class="heading navigation col-2"><a href="#">Best In Slot List</a></div>
-            <div class="heading navigation col-2"><a href="#">Spell Power Calculator</a></div>
-            <div class="col-1"></div>
-            <div class="heading col-2">Hey there stranger</div>
-        </div>
-    </div>
-    <div class="container introduction">
-        <h2>Welcome Home&nbsp;<img src="./view/shadowwordpain.jpg" width="30" height="30" alt="Shadow Word Pain Icon" class="introicon"></h2><hr/>
-        <p>This is our shadowy lair. We will be journeying into the dark portal soon enough, and lucky for you Blizzard has blessed us into viability. I plan on playing a Draenei Shadow Priest in The Burning Crusade Classic, and I can't wait for the prepatch to hit live servers. This website is dedicated to all lovers of the world of shadows (no warlocks allowed, sorry). Join me as I attempt to create the best encapsulation of all things Shadow Priest to help your journey into The Burning Crusade Classic.
-        </p>
-    </div>
-</body>
-</html>
+<?php 
+    $page = "home";
+    include('./view/header.php');
+?>
+
+<div class="container introduction col-5">
+    <h2>Welcome Home&nbsp;<img src="./view/shadowwordpain.jpg" width="30" height="30" alt="Shadow Word Pain Icon" class="introicon"></h2><hr/>
+    <p>This is our shadowy lair. We will be journeying into the dark portal soon enough, and lucky for you Blizzard has blessed us into viability. I plan on playing a Draenei Shadow Priest in The Burning Crusade Classic, and I can't wait for the prepatch to hit live servers. This website is dedicated to all lovers of the world of shadows (no warlocks allowed, sorry). Join me as I attempt to create the best encapsulation of all things Shadow Priest to help your journey into The Burning Crusade Classic.
+    </p>
+</div>
+    
+<?php include('./view/footer.php');?>
---
+++ view/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./view/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Courgette">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <?php 
+        switch($page) {
+            case 'home':
+                echo "<title>Into the Void</title>";
+            case 'bislist':
+                echo "<title>Best in Slot List</title>";
+            case 'dpscalculator':
+                echo "<title>DPS Calculator</title>";
+        }
+    ?>
+</head>
+<body>
+    <div class="container-fluid navrow">
+        <div class="row">
+            <img src="./view/shadowformicon.jpg" width="37" height="35" margin-left="0" alt="Shadow Form Icon">
+            <div class="heading navigation col-2" id="active"><a href="#" class="stretched-link">Home</a></div>
+            <div class="heading navigation col-2"><a href="#">PVE Guides</a></div>
+            <div class="heading navigation col-2"><a href="#">Best In Slot List</a></div>
+            <div class="heading navigation col-2"><a href="#">DPS Calculator</a></div>
+            <div class="col-1"></div>
+            <div class="heading col-2">Hey there stranger</div>
+        </div>
+    </div>
