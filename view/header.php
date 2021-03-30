fabc1e45f66a8831a57b761d79167b78be37d1b1
Cody Hinz <codyhinz@gmail.com>
2021-03-30 14:20:05 -0400

    New improvements, soon to be finished home page

--- view/header.php
+++ view/header.php
@@ -10,10 +10,13 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="./model/jsfunctions.js"></script>
     <?php 
         switch($page) {
             case 'home':
                 echo "<title>Into the Void</title>";
+            case 'pveguide':
+                echo "<title>PVE Guide</title>";
             case 'bislist':
                 echo "<title>Best in Slot List</title>";
             case 'dpscalculator':
@@ -25,11 +28,11 @@
     <div class="container-fluid navrow">
         <div class="row">
             <img src="./view/shadowformicon.jpg" width="37" height="35" margin-left="0" alt="Shadow Form Icon">
-            <div class="heading navigation col-2" id="active"><a href="#" class="stretched-link">Home</a></div>
-            <div class="heading navigation col-2"><a href="#">PVE Guides</a></div>
-            <div class="heading navigation col-2"><a href="#">Best In Slot List</a></div>
-            <div class="heading navigation col-2"><a href="#">DPS Calculator</a></div>
+            <div class="heading navigation col-2" id="<?php if ($page == "home"){ echo "active";}?>"><a href="#" class="stretched-link">Home</a></div>
+            <div class="heading navigation col-2" id="<?php if ($page == "pveguide"){ echo "active";}?>"><a href="#">PVE Guides</a></div>
+            <div class="heading navigation col-2" id="<?php if ($page == "bislist"){ echo "active";}?>"><a href="#">Best In Slot List</a></div>
+            <div class="heading navigation col-2" id="<?php if ($page == "dpscalulator"){ echo "active";}?>"><a href="#">DPS Calculator</a></div>
             <div class="col-1"></div>
-            <div class="heading col-2">Hey there stranger</div>
+            <div class="heading col-2"></div>
         </div>
     </div>
