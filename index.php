62dc98ae2e8239a7b60ed15b9b01c79ccbecc6e2
Cody Hinz <codyhinz@gmail.com>
2021-03-30 12:42:26 -0400

    Added sound for page transition

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     include('./view/header.php');
 ?>
 
-<div class="container introduction col-5">
+<div class="container introduction col-5 col-md-6">
     <h2>Welcome Home&nbsp;<img src="./view/shadowwordpain.jpg" width="30" height="30" alt="Shadow Word Pain Icon" class="introicon"></h2><hr/>
     <p>This is our shadowy lair. We will be journeying into the dark portal soon enough, and lucky for you Blizzard has blessed us into viability. I plan on playing a Draenei Shadow Priest in The Burning Crusade Classic, and I can't wait for the prepatch to hit live servers. This website is dedicated to all lovers of the world of shadows (no warlocks allowed, sorry). Join me as I attempt to create the best encapsulation of all things Shadow Priest to help your journey into The Burning Crusade Classic.
     </p>
--- view/footer.php
+++ view/footer.php
@@ -1,4 +1,5 @@
-
-
+<footer>
+    <div class="container-fluid text-center font-weight-light">&#169;IntotheVoid 2021</div>
+</footer>
 </body>
 </html>
