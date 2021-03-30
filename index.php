54cc44655cb92726d7200a2a2d013be6c82ce58d
Cody Hinz <codyhinz@gmail.com>
2021-03-30 14:36:07 -0400

    Homepage almost done

--- index.php
+++ index.php
@@ -2,15 +2,22 @@
     $page = "home";
     include('./view/header.php');
 ?>
-
-    <div class="container introduction col-4 col-md-6">
+<div class="row container-fluid">
+    <div class="introduction col-5">
         <h2>Welcome Home&nbsp;<img src="./view/shadowwordpain.jpg" width="30" height="30" alt="Shadow Word Pain Icon" class="introicon"></h2><hr/>
         <p>This is our shadowy lair, and soon we will be journeying into the dark portal again. Lucky for you Blizzard has blessed us into viability. I plan on playing a Draenei Shadow Priest in The Burning Crusade Classic, and I can't wait for the prepatch to hit live servers. This website is dedicated to all lovers of the world of shadows (no warlocks allowed, sorry). Join me as I attempt to create the best encapsulation of all things Shadow Priest to help your journey into The Burning Crusade Classic.
         </p>
     </div>
-    <div class="container introduction col-4 col-md-4">
-        <h2>Who are you?</h3>
-        <p>Fair enough, I am a long time private server enjoyer with many Sunwell Plateau clears on various servers(most of which as a shadow priest). I don't claim to be the best at anything, but I have done this progression about a dozen times and I'm here to share some wisdom. My only trackable achievements are my parses in classic. I have a rank 1 parse as a fury warrior on classic, and I hope to be the rank 1 Alliance shadow priest in Classic TBC.
-        </p>
+    <div class="introduction col-5">
+        <h2>Who are you?</h3><hr/>
+        <div class="row">
+            <div class="col-9">
+                <p>Fair enough, I am a long time private server enjoyer with many Sunwell Plateau clears on various servers(most of which as a shadow priest). I don't claim to be the best at anything, but I have done this progression about a dozen times. My only trackable achievements are my parses in classic. I have a rank 1 parse as a fury warrior on classic, and I hope to be the rank 1 Alliance shadow priest in Classic TBC.</p>
+            </div>
+            <div class="col-2">
+                <img src="./view/rank1.jpg" width="120" height="110" alt="Rank 1 Parse" class="parseicon">
+            </div>
+        </div>    
     </div>
+</div>
 <?php include('./view/footer.php');?>
