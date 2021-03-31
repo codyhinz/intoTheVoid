b52ca817fb5be7de9c3febb4986a53ead292d137
Cody Hinz <codyhinz@gmail.com>
2021-03-30 21:38:39 -0400

    First test launch

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,8 @@
-<div class="container col-11 cookiespopup">
-    <p class="text-center">This website uses coookies to enchance your viewing and surfing experience. This is your obligatory notice that we use cookies on this website.🍪</p>
+<div class="container cookiespopup">
+    <div class="row col-10">
+        <p class="text-center">This website uses cookies to enhance your viewing and surfing experience. This is your obligatory notice that we use cookies on this website.🍪</p>
+    </div>
+    <div class="col-1 float-right" id="xicon">❌</div>
 </div>
 <footer>
     <div class="container text-center font-weight-light">&#169;IntotheVoid 2021</div>
