c8f122d2403c56d617163a77052161ffa53fe178
Cody Hinz <codyhinz@gmail.com>
2021-03-30 20:28:53 -0400

    Updated style a bit, added switch for different styling

--- view/footer.php
+++ view/footer.php
@@ -1,3 +1,6 @@
+<div class="container col-11 cookiespopup">
+    <p class="text-center">This website uses coookies to enchance your viewing and surfing experience. This is your obligatory notice that we use cookies on this website.🍪</p>
+</div>
 <footer>
     <div class="container text-center font-weight-light">&#169;IntotheVoid 2021</div>
 </footer>
--- view/header.php
+++ view/header.php
@@ -10,7 +10,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="./model/jsfunctions.js"></script>
     <?php 
         switch($page) {
             case 'home':
@@ -27,12 +26,46 @@
 <body>
     <div class="container-fluid navrow">
         <div class="row">
-            <img src="./view/shadowformicon.jpg" width="37" height="35" margin-left="0" alt="Shadow Form Icon">
+            <img src="./view/shadowformicon.jpg" width="40" height="41" margin-left="0" alt="Shadow Form Icon">
             <div class="heading navigation col-2" id="<?php if ($page == "home"){ echo "active";}?>"><a href="#" class="stretched-link">Home</a></div>
             <div class="heading navigation col-2" id="<?php if ($page == "pveguide"){ echo "active";}?>"><a href="#">PVE Guides</a></div>
             <div class="heading navigation col-2" id="<?php if ($page == "bislist"){ echo "active";}?>"><a href="#">Best In Slot List</a></div>
             <div class="heading navigation col-2" id="<?php if ($page == "dpscalulator"){ echo "active";}?>"><a href="#">DPS Calculator</a></div>
             <div class="col-1"></div>
-            <div class="heading col-2"></div>
+            <div class="heading col-2">
+                <div class="container">
+                    <label id="switch" class="switch">
+                        <input type="checkbox" onchange="toggleTheme()" id="slider">
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+            <script>
+                // function to set a given theme/color-scheme
+                function setTheme(themeName) {
+                    localStorage.setItem('theme', themeName);
+                    document.documentElement.className = themeName;
+                }
+
+                // function to toggle between light and dark theme
+                function toggleTheme() {
+                    if (localStorage.getItem('theme') === 'theme-dark') {
+                        setTheme('theme-light');
+                    } else {
+                        setTheme('theme-dark');
+                    }
+                }
+
+                // Immediately invoked function to set the theme on initial load
+                (function () {
+                    if (localStorage.getItem('theme') === 'theme-dark') {
+                        setTheme('theme-dark');
+                        document.getElementById('slider').checked = false;
+                    } else {
+                        setTheme('theme-light');
+                    document.getElementById('slider').checked = true;
+                    }
+                })();
+            </script>
+            </div>
         </div>
     </div>
