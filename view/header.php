98684e202fce2f10100dd3e60858d24195a4585c
Cody Hinz <codyhinz@gmail.com>
2021-03-30 21:46:41 -0400

    Added website icon

--- view/header.php
+++ view/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="./view/style.css">
+    <link rel="icon" href="./view/shadowformicon.jpg">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Courgette">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
