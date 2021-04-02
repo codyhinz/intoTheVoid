c33413c0d81f8fd52934f1cb2c52761a677005cb
Cody Hinz <codyhinz@gmail.com>
2021-04-02 17:41:15 -0400

    Fixed major js bug

--- model/functions.js
+++ model/functions.js
@@ -23,9 +23,9 @@ function toggleTheme() {
 // Immediately invoked function to set the theme on initial load
 (function () {
   if (localStorage.getItem('theme') === 'theme-dark') {
-      setTheme('theme-dark');
-  } else {
       setTheme('theme-light');
+  } else {
+      setTheme('theme-dark');
   }
 })();
 
