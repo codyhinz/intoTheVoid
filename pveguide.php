8efd1323c5bc9528822c914cb832d6b9934a13ea
Cody Hinz <codyhinz@gmail.com>
2021-04-03 00:09:56 -0400

    Adding to pve page

--- pveguide.php
+++ pveguide.php
@@ -20,23 +20,36 @@
             </p>
         </div>
     </div>
-    <div class="row col-12 mt-5">
+    <div class="row col-12 mt-5 mb-5">
         <div class="col-6">
-            <h5 class="mb-3 text-primary"><img src="./view/images/alliance.png" width="45" height="40">&nbsp;Alliance</h5>
+            <h5 class="mb-3 text-primary"><img src="./view/images/alliance.png" width="35" height="30">&nbsp;Alliance</h5>
             <ul class="ml-2">
-                <li class="font-weight-bold text-primary">Draenei</li>
-                <li>Dwarf</li>
-                <li>Human</li>
-                <li>Night Elf</li>
+                <li class="font-weight-bold text-primary draenei">Draenei</li>
+                <li class="dwarf">Dwarf</li>
+                <li class="human">Human</li>
+                <li class="nightelf">Night Elf</li>
             </ul>
         </div>
         <div class="col-6">
-            <h5 class="mb-3 text-danger"><img src="./view/images/horde.png" width="50" height="45">Horde</h5>
+            <h5 class="mb-3 text-danger"><img src="./view/images/horde.png" width="40" height="35">Horde</h5>
             <ul class="ml-2">
-                <li class="font-weight-bold text-danger">Undead</li>
-                <li>Blood Elf</li>
-                <li>Troll</li>
+                <li class="font-weight-bold text-danger undead mt-2">Undead</li>
+                <li class="bloodelf">Blood Elf</li>
+                <li class="troll">Troll</li>
             </ul>
+        </div>
+    </div>
+    <div class="col-12 mt-5">
+        <div class="mt-5"><h2 class="text-primary"><img src="./view/images/alliance.png" width="50" height="40">&nbsp;Alliance</h2></div>
+    </div>
+    <div class="row col-12">
+        <div class="col-xl-7 col-lg-7 col-md-7 mt-2">
+            <p>In terms of PVE the alliance have a massive advantage; the draenei.
+                Bringing their 1% spell hit into the equation, this means you can gear even less for spell hit.
+                Inspiring presence is by far and away the best racial for shadow priests</p>
+        </div>
+        <div class="col-xl-5 col-lg-5 col-md-5">
+            <img src="./view/images/inspiringpresence.png" class="img-fluid">
         </div>
     </div>
 </div>
