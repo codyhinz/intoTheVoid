03f887a85c1a423df081f37656c026a1ca733386
Cody Hinz <codyhinz@gmail.com>
2021-04-03 14:52:22 -0400

    Adding contetn

--- pveguide.php
+++ pveguide.php
@@ -22,7 +22,7 @@
     </div>
     <div class="row col-12 mt-5 mb-5">
         <div class="col-6">
-            <h5 class="mb-3 text-primary"><img src="./view/images/alliance.png" width="35" height="30">&nbsp;Alliance</h5>
+            <h5 class="mb-3 text-primary"><img src="./view/images/alliance.png" width="35" height="30" alt="Alliance Icon">&nbsp;Alliance</h5>
             <ul class="ml-2">
                 <li class="font-weight-bold text-primary draenei">Draenei</li>
                 <li class="dwarf">Dwarf</li>
@@ -31,7 +31,7 @@
             </ul>
         </div>
         <div class="col-6">
-            <h5 class="mb-3 text-danger"><img src="./view/images/horde.png" width="40" height="35">Horde</h5>
+            <h5 class="mb-3 text-danger"><img src="./view/images/horde.png" width="40" height="35" alt="Horde Icon">Horde</h5>
             <ul class="ml-2">
                 <li class="font-weight-bold text-danger undead mt-2">Undead</li>
                 <li class="bloodelf">Blood Elf</li>
@@ -40,19 +40,84 @@
         </div>
     </div>
     <div class="col-12 mt-5">
-        <div class="mt-5"><h2 class="text-primary"><img src="./view/images/alliance.png" width="50" height="40">&nbsp;Alliance</h2></div>
+        <div class="mt-5"><h2 class="text-primary"><img src="./view/images/alliance.png" width="50" height="40" alt="Alliance Icon">&nbsp;Alliance</h2></div>
     </div>
     <div class="row col-12">
         <div class="col-xl-7 col-lg-7 col-md-12 mt-2">
-            <h5 class="font-weight-bold">Draenei:</h5>
+            <h5 class="font-weight-bold text-primary">Draenei:</h5>
+            <p>Chastise is an interesting ability, but I doubt you will find much use for it in PVE.
+            In PVP this is a pretty decent ability. A two second root can make or break an arena match for you.</p>
+        </div>
+        <div class="col-xl-5 col-lg-5 col-md-12">
+            <img src="./view/images/chastise.png" class="img-fluid" alt="Chastise Icon">
+        </div>
+    </div><hr/>
+    <div class="row col-12">
+        <div class="col-xl-7 col-lg-7 col-md-12 mt-2">
+            <p>Gemcutting is a fairly weak passive.
+            As a shadow priest you will most likely want to be Tailoring/Enchanting.
+            Jewelcrafting is a fairly weak profession as a shadow priest pushing their PVE potential.</p>
+        </div>
+        <div class="col-xl-5 col-lg-5 col-md-12 mt-2">
+            <img src="./view/images/gemcutting.png" class="img-fluid" alt="Gemcutting Icon">
+        </div>
+    </div><hr/>
+    <div class="row col-12">
+        <div class="col-xl-7 col-lg-7 col-md-12 mt-2">
+            <p>Gift of the Naaru is probably the weakest link of all of the draenei racials.
+            It's a very minimal heal over time effect at level 70 at least.
+            I do think it's better than most people think though at least for leveling.
+            Unfortunately it doesn't scale very well with levels or spellpower.
+            I did find myself using this and throwing a max rank renew before taking on more difficult mobs solo.</p>
+        </div>
+        <div class="col-xl-5 col-lg-5 col-md-12 mt-2">
+            <img src="./view/images/giftofthenaaru.png" class="img-fluid" alt="Gift of the Naaru Icon">
+        </div>
+    </div><hr/>
+    <div class="row col-12 mb-5">
+        <div class="col-xl-7 col-lg-7 col-md-12 mt-2">
             <p>In terms of PVE the alliance have a massive advantage; the draenei.
                 Bringing their 1% spell hit into the equation, this means you can gear even less for spell hit.
-                Inspiring presence is by far and away the best racial for shadow priests.</p>
+                Inspiring presence is by far and away the best racial for shadow priests
+                (alot of raids will even form their groups around your hit chance group passive).</p>
         </div>
-        <div class="col-xl-5 col-lg-5 col-md-12 mt-4">
-            <img src="./view/images/inspiringpresence.png" class="img-fluid">
+        <div class="col-xl-5 col-lg-5 col-md-12">
+            <img src="./view/images/inspiringpresence.png" class="img-fluid mt-2" alt="Inspiring Presence Icon">
+        </div>
+    </div><hr/>
+    <div class="row col-12">
+        <div class="col-xl-7 col-lg-7 col-md-12 mt-2">
+            <p>This is a surpisingly helpful passive resistance.
+            There is ALOT of shadow damage in The Burning Crusade.
+            Obviously 10 shadow resistance isn't much,
+            but all classes get one and for BC I would definitely consider this the best.</p>
+        </div>
+        <div class="col-xl-5 col-lg-5 col-md-12">
+            <img src="./view/images/shadowresistance.png" class="img-fluid" alt="Shadow Resistance Icon">
+        </div>
+    </div><hr/>
+    <div class="row col-12">
+        <div class="col-xl-7 col-lg-7 col-md-12 mt-2">
+            <p>Ah, one of my favorite cooldowns.
+            Symbol of Hope is incredible, the scaling works very well in terms of leveling.
+            This will be a very powerful cooldown for you and your group members.
+            You will truly embrace the mana-battery mentality with this.</p>
+        </div>
+        <div class="col-xl-5 col-lg-5 col-md-12">
+            <img src="./view/images/symbolofhope.png" class="img-fluid" alt="Symbol of Hope Icon">
         </div>
     </div>
+    <div class="mb-5 mt-5 row col-12"></div>
+    <div class="row col-12 mt-5 mb-5">
+        <div class="col-xl-7 col-lg-7 col-md-12 mt-2">
+            <h5 class="font-weight-bold text-primary">Dwarf:</h5>
+            <p>The king of classic priest has fallen.
+            I'd even go as far to say is the weakest priest race in the alliance.</p>
+        </div>
+        <div class="col-xl-5 col-lg-5 col-md-12 mb-1">
+            <img src="./view/images/inspiringpresence.png" class="img-fluid mt-4" alt="Inspiring Presence Icon">
+        </div>
+    </div><hr/>
 </div>
 
 <?php include('./view/footer.php');?>
