6bd7e678f14d4c774dc05762a2517fe4183f5559
Cody Hinz <codyhinz@gmail.com>
2021-04-08 19:30:58 -0400

    Added defer to js scripts

--- view/header.php
+++ view/header.php
@@ -11,8 +11,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="./model/functions.js"></script>
-    <script src="./model/theme.js"></script>
+    <script src="./model/functions.js" defer></script>
+    <script src="./model/theme.js" defer></script>
     <?php
         switch($page) {
             case 'home':
